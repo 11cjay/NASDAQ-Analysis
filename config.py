6e715967784158803883762285275f9cc77bf3a3
11cjay <92046636+11cjay@users.noreply.github.com>
2025-10-26 02:27:34 +0000

    Add placeholders for API configuration
    Added configuration placeholders for API key, password, and username.

---
+++ config.py
@@ -0,0 +1,6 @@
+# api_key = ........(YOUR API KEY HERE)
+# password = ....... (YOUR PASSWORD)
+# username = ....... (YOUR USERNAME E.G. ADMIN1)
+
+
+# THIS FILE IS REQUIRED, MAIN.PY WILL FAIL WITHOUT IT
